fe8d065bdfc014ff2ddb75bdf4ef4122f5a4d5cc
Catherine <83070087+vaahtokarkkeja@users.noreply.github.com>
2022-11-24 17:14:15 +0300

    Добавила слайдер категорий

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
-const swiper = new Swiper('.swiper', {
+const swiperStories = new Swiper('.stories-slider', {
     slidesPerView: 'auto',
-  });
+});
+const swiperMainCategories = new Swiper('.main-cat-slider', {
+    slidesPerView: 1,
+    navigation: {
+        nextEl: '.main-cat-slider .swiper-button-next',
+        prevEl: '.main-cat-slider .swiper-button-prev',
+      },
+    breakpoints: {
+        1025: {
+          slidesPerView: 2
+        }
+      }
+});
