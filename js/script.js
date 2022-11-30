690f8af7e5ff3a648b4fc72bcc7cb59451473f7f
Catherine <83070087+vaahtokarkkeja@users.noreply.github.com>
2022-11-30 22:04:01 +0300

    Добавила окно поиска и логику для него

--- js/script.js
+++ js/script.js
@@ -97,8 +97,9 @@ window.addEventListener("scroll", function () {
     header.classList.add("header-main");
   }
 });
-// jquery menu
+
 $(function () {
+  // jquery menu
   $(".js-menu-trigger").on("click", function () {
     $(".js-menu").toggleClass("menu-active");
     $(".page").toggleClass("no-scroll");
@@ -108,6 +109,8 @@ $(function () {
     $(".submenu-parent").removeClass("submenu-parent");
     $(".js-menu").removeClass("submenu-active");
     $(".on-top").removeClass("on-top");
+
+    return false;
   });
   $(".js-submenu-trigger").on("click", function () {
     if ($(".js-menu").is(".menu-active")) {
@@ -119,6 +122,8 @@ $(function () {
       $(this).closest(".js-submenu-active").addClass("submenu-parent");
       $(this).closest(".js-submenu-active div").addClass("hide-siblings");
     }
+
+    return false;
   });
 
   $(".js-back-trigger").on("click", function () {
@@ -131,5 +136,88 @@ $(function () {
       $(".js-menu").removeClass("submenu-active");
       $(".header-categories-item").removeClass("hide-siblings");
     }
+    return false;
   });
-});
+  // jquery search front
+  $('.header-search-trigger').on('click', function() {
+    $('.header-search').addClass('header-search-open');
+    $(".page").toggleClass("no-scroll");
+    return false;
+  });
+
+  $('html').on('click', function(e) {
+    if(($('.header-search-open').length) && !$(e.target).closest('.header-search-open').length){
+      $('.header-search-open').removeClass('header-search-open');
+      $(".page").toggleClass("no-scroll");
+      $('.header-search-prev').hide();
+      $('.header-search-result').removeClass('header-search-result-open');
+      $('.header-search-input').val("");
+      if($(window).width() <= 1024) {
+        $('.header-search-reset').removeClass('header-search-reset-mobile');
+      }
+    }
+    return false;
+  });
+
+  $('.header-search-back').on('click', function() {
+      $('.header-search-open').removeClass('header-search-open');
+      $(".page").toggleClass("no-scroll");
+      $('.header-search-prev').hide();
+      $('.header-search-result').removeClass('header-search-result-open');
+      $('.header-search-input').val("");
+      $('.header-search-reset').removeClass('header-search-reset-mobile');
+      return false;
+  });
+
+  $('.header-search-input').focus(function() {
+    if(!$('.header-search-input').val() && $('.header-search-prev').is(':hidden') && $('.header-search-prev-item').length) {
+      $('.header-search-prev').show();
+    }
+  });
+
+  $('.header-search-reset').on('click', function() {
+    if(!$('.header-search-input').val()) {
+      $('.header-search-open').removeClass('header-search-open');
+      $(".page").toggleClass("no-scroll");
+      $('.header-search-prev').hide();
+      $('.header-search-result').removeClass('header-search-result-open');
+    } else {
+      $('.header-search-input').val("");
+      if($('.header-search-prev-item').length) {
+        $('.header-search-prev').show();
+      }
+      $('.header-search-result').removeClass('header-search-result-open');
+      if($(window).width() <= 1024) {
+        $('.header-search-reset').removeClass('header-search-reset-mobile');
+      }
+    }
+    return false;
+  });
+
+  $('.header-search-prev-reset').on('click', function() {
+    $(this).closest('.header-search-prev-item').remove();
+    if(!$('.header-search-prev-item').length) {
+      $('.header-search-prev').hide();
+    }
+    return false;
+  })
+
+  $('.header-search-input').on('input', function() {
+    if($(this).val()) {
+      $('.header-search-result').addClass('header-search-result-open');
+      $('.header-search-prev').hide();
+      if($(window).width() <= 1024) {
+        $('.header-search-reset').addClass('header-search-reset-mobile');
+      }
+    } else {
+      if($('.header-search-prev-item').length) {
+        $('.header-search-result').removeClass('header-search-result-open');
+        $('.header-search-prev').show();
+      }
+      if($(window).width() <= 1024) {
+        $('.header-search-reset').removeClass('header-search-reset-mobile');
+      }
+    }
+  })
+
+})
